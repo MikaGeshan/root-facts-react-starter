7ba1bed020848ec825614fae2e610bd788b9fa96
mika.arip <mika.arip@quadrant-si.id>
2026-05-11 20:11:50 +0700

    fix after deploy

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'auto',
       includeAssets: ['favicon.ico', 'icons/*.png', 'model/*'],
       manifest: {
         name: 'RootFacts - AI Vegetable Fun Facts',
